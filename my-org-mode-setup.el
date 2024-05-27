cfd1a6d85d6f38081bd7159c6a048543531d6300
laurent <laurent_pinson@hotmail.com>
2024-05-27 17:56:31 +0700

    Update my-org-mode-setup.el

--- my-org-mode-setup.el
+++ my-org-mode-setup.el
@@ -8,10 +8,3 @@
         (message "let's go!")
         (sit-for 2))
     (message "my-org-mode-setup-function is not defined. Check your package setup!")))
-
-;; DOOMDIR/config.el
-(use-package! my-org-mode-setup
-  :commands my-org-mode-setup-function
-  :config
-  (map! :map org-mode-map
-        "C-d o" #'my-org-mode-setup-function))
