3eeba555b8cee38f62c0868a58105bdc92ea3428
laurent <laurent_pinson@hotmail.com>
2024-05-27 11:00:21 +0700

    Create my-org-mode-setup.el

---
+++ my-org-mode-setup.el
@@ -0,0 +1,8 @@
+(defun my-org-mode-setup ()
+  "Custom setup for Org mode."
+  (interactive)
+  (message "Org mode is now active!")
+  (sit-for 2)
+  (message "let's go!")
+  (sit-for 2))
+(global-set-key (kbd "C-c o") 'my-org-mode-setup)
