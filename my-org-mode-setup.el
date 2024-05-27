ed233eec89a3dc1e31a984799cd3174bfcc9e2c8
laurent <laurent_pinson@hotmail.com>
2024-05-27 17:51:59 +0700

    Update my-org-mode-setup.el

--- my-org-mode-setup.el
+++ my-org-mode-setup.el
@@ -1,9 +1,17 @@
-(defun my-org-mode-setup ()
+(defun my-org-mode-setup-function ()
   "Custom setup for Org mode."
   (interactive)
-  (message "Org mode is now active!")
-  (sit-for 2)
-  (message "let's go!")
-  (sit-for 2))
-(global-set-key (kbd "C-c o") 'my-org-mode-setup)
-(add-hook 'org-mode-hook #'my-org-mode-setup)
+  (if (fboundp 'my-org-mode-setup-function)
+      (progn
+        (message "Org mode is now active!")
+        (sit-for 2)
+        (message "let's go!")
+        (sit-for 2))
+    (message "my-org-mode-setup-function is not defined. Check your package setup!")))
+
+;; DOOMDIR/config.el
+(use-package! my-org-mode-setup
+  :commands my-org-mode-setup-function
+  :config
+  (map! :map org-mode-map
+        "C-d o" #'my-org-mode-setup-function))
